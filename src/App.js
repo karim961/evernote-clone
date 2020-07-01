8489e7c93821de4462b149884dfc4aa2e9d67b65
Karim Abbas <32097461+karim961@users.noreply.github.com>
2020-07-02 01:50:28 +0300

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            selectNoteIndex: null,
+            selectedNote: null,
+            notes: null
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+
+    componentDidMount = () => {
+        firebase.firestore()
+            .collection('notes')
+            .onSnapshot(serverUpdate => {
+                const notes = serverUpdate.docs.map(_doc =>{
+                    const data = _doc.data();
+                    data['id']=_doc.id;
+                    return data;
+                });
+                console.log(notes);
+                this.setState({notes:notes});
+            })
+    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBhXXg8ArpiVRWUjmqsIdM3ygCItloiNjs",
+    authDomain: "evernote-clone-6a96d.firebaseapp.com",
+    databaseURL: "https://evernote-clone-6a96d.firebaseio.com",
+    projectId: "evernote-clone-6a96d",
+    storageBucket: "evernote-clone-6a96d.appspot.com",
+    messagingSenderId: "915437558234",
+    appId: "1:915437558234:web:0f1c2cdedd5425ec44c8e3"
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
